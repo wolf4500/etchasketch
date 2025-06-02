74fc4fcbf5ef61f33f9d6e19169254b22bcb49e8
aaron <aarondang4500@gmail.com>
2025-06-02 11:28:49 -0500

    Creates grid of fixed size 16x16.

---
+++ script.js
@@ -0,0 +1,32 @@
+const gridContainer = document.querySelector(".grid-container");
+
+// Creates the grid cells
+for (let i = 0; i < 16; i++) {
+    for (let n = 0; n < 16; n++) {
+        let div = document.createElement('div');
+        const size = (500/16);
+        div.setAttribute("style", `border: 1px solid black; width: ${size}px; height: ${size}px;`);
+        div.setAttribute("class", `cell`);
+        gridContainer.append(div);
+    }
+}
+
+// Flag to check if mouse is clicked down
+let isMouseDown = false;
+
+// Adds background color to the first cell that is clicked down on, otherwise cell is blank.
+document.addEventListener("mousedown", (event) => {
+    isMouseDown = true;
+    event.target.style.backgroundColor = "black";
+})
+
+document.addEventListener("mouseup", () => {
+    isMouseDown = false;
+})
+
+// Adds background color to cells that are being hovered over only if mouse is clicked down.
+gridContainer.addEventListener('mouseover', (event) => {
+    if (event.target.classList.contains("cell") && isMouseDown) {
+        event.target.style.backgroundColor = "black";
+    }
+})
